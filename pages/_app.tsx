916ed06c1159626d6a73a83215d3fc6a347b9763
Gabriel R <gabriel1706.com@gmail.com>
2023-12-22 14:25:35 -0300

    responsive fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
       <link rel="icon" href="/favicon.ico" sizes="any" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
       <main className={poppins.className}>
         <Header />
