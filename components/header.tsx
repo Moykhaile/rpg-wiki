ede25f7f43cf0a1c7914251459efa6e71bcc3e03
Gabriel R <gabriel1706.com@gmail.com>
2023-12-22 11:53:28 -0300

    remove go back to menu arrow

--- components/header.tsx
+++ components/header.tsx
@@ -2,14 +2,11 @@ import header from '@/styles/header.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { usePathname } from 'next/navigation'
-
 export default function Header() {
-    const pathname = usePathname()
     return (
         <Link className={header.header} href="/">
             <Image className={header.img} src="./Logo.png" alt="Ir ao menu" width={500} height={500} />
-            <div className={header.title}>Rebelião sob Discórdia {pathname != '/' ? ' ←' : null}</div>
+            <div className={header.title}>Rebelião sob Discórdia</div>
         </Link>
     );
 }
