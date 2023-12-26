1030923b3eb21581176ac491decea2cb4953195b
Gabriel R <gabriel1706.com@gmail.com>
2023-12-26 17:44:21 -0300

    logo homepage fix

--- components/header.tsx
+++ components/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     console.log(path);
     return (
         <Link className={header.header} href="/">
-            <Image className={header.img} src={path == '' ? "./Logo.png" : "../Logo.png"} alt="Ir ao menu" width={500} height={500} />
+            <Image className={header.img} src={path == '/' ? "./Logo.png" : "../Logo.png"} alt="Ir ao menu" width={500} height={500} />
             <div className={header.title}>Rebelião sob Discórdia</div>
         </Link>
     );
