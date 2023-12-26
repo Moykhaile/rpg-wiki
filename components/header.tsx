cbc7b1992e97194d20370089ba73f4716b42cb2d
Gabriel R <gabriel1706.com@gmail.com>
2023-12-26 12:31:14 -0300

    wiki image border

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Header() {
     return (
         <Link className={header.header} href="/">
-            <Image className={header.img} src="./Logo.png" alt="Ir ao menu" width={500} height={500} />
+            <Image className={header.img} src="/./Logo.png" alt="Ir ao menu" width={500} height={500} />
             <div className={header.title}>Rebelião sob Discórdia</div>
         </Link>
     );
