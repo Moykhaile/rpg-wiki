7bc8eff7ad20d23947867226297a38c489cfcceb
Gabriel R <gabriel1706.com@gmail.com>
2023-12-26 12:33:56 -0300

    img border

--- pages/Localizações/Algihand.tsx
+++ pages/Localizações/Algihand.tsx
@@ -9,7 +9,7 @@ export default function Algihand() {
     return (
         <main className={Wiki.main}>
             <div className={Wiki.desc}>
-                <TituloWiki titulo="Algihand" desc="" />
+                <TituloWiki titulo="Algihand" desc="Continente" />
                 <ItemWiki titulo="Aumento no Valor de Habilidade." desc="Seu valor de Destreza aumenta em 2." />
                 <ItemWiki titulo="Idade." desc="Assim como os humanos, avianos chegam a idade adulta no final da adolescência e podem viver teoricamente até cerca de 80 anos. É claro, a maioria encontram uma morte gloriosa (ou sem glória) bem antes desse ponto." />
                 <h2>Falcão</h2>
@@ -17,7 +17,7 @@ export default function Algihand() {
                 <ItemWiki titulo="Benção de Kefnet." desc="Você pode adicionar metade do seu bônus de proficiência, arredondado para baixo, em qualquer teste de Inteligência que você faça que já não inclua seu bônus de proficiência." />
             </div>
             <div className={Wiki.images}>
-                <Image className={Wiki.img} src="/./RPG map.png" width={1600} height={1200} alt="Um mapa de Algihand" />
+                <Image className={Wiki.img} src="./RPG map.png" width={1600} height={1200} alt="Um mapa de Algihand" />
             </div>
         </main>
     )
