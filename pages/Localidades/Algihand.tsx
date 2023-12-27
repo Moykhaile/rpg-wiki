13d5bff78276fd949d08a3cde238fda7796da0a7
Gabriel R <gabriel1706.com@gmail.com>
2023-12-27 12:06:16 -0300

    Algihand map update

--- pages/Localidades/Algihand.tsx
+++ pages/Localidades/Algihand.tsx
@@ -7,7 +7,7 @@ import Wiki from "@/styles/wiki.module.css"
 
 export default function Algihand() {
     return (
-        <main className={Wiki.main}>
+        <main className={Wiki.main && Wiki.column}>
             <div className={Wiki.desc}>
                 <TituloWiki titulo="Algihand" desc="Continente (Pop. 3500)" />
                 <ItemWiki titulo="Largura." desc="21km" />
@@ -15,9 +15,9 @@ export default function Algihand() {
                 <ItemWiki titulo="Área." desc="10.752km" />
             </div>
             <div className={Wiki.images}>
-                <Image className={Wiki.img} src="../RPG map.png" width={1600} height={1200} alt="Um mapa de Algihand" />
+                <Image className={Wiki.img} src="../Mapa.png" width={1600} height={1200} alt="Um mapa de Algihand" />
                 <ItemWiki titulo=">" desc="O mapa de Algihand, atualizado conforme a aventura avança" />
             </div>
-        </main>
+        </main >
     )
 }
