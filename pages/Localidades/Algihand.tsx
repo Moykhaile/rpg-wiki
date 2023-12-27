5424c02c3910f122d231be2d72b7cf9303da5719
Gabriel R <gabriel1706.com@gmail.com>
2023-12-27 12:30:56 -0300

    linguas algihand

--- pages/Localidades/Algihand.tsx
+++ pages/Localidades/Algihand.tsx
@@ -13,6 +13,7 @@ export default function Algihand() {
                 <ItemWiki titulo="Largura." desc="21km" />
                 <ItemWiki titulo="Altura." desc="22,5km" />
                 <ItemWiki titulo="Área." desc="10.752km" />
+                <ItemWiki titulo="Idiomas." desc="Algihani, Inglês, Anão, Grunge, Ta'agra, Élfico, Dracônico, Halfling, Aviano, Gnômico, Orc, Infernal" />
             </div>
             <div className={Wiki.images}>
                 <Image className={Wiki.img} src="../Mapa.png" width={1600} height={1200} alt="Um mapa de Algihand" />
