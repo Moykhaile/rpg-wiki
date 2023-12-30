98817fd3da5a48428e24ab5995df51a36910e0f2
Gabriel R <gabriel1706.com@gmail.com>
2023-12-30 00:45:52 -0300

    fixx

--- pages/Localidades/Matnnahoma.tsx
+++ pages/Localidades/Matnnahoma.tsx
@@ -16,22 +16,22 @@ export default function Matnnahoma() {
             </div>
             <div className={Wiki.images}>
                 <Link href="../Matnnahoma/Matnnahoma.png">
-                    <Image className={Wiki.img} src="../Matnnahoma/Matnnahoma.png" width={4095} height={4095} alt="Um mapa de Algihand" />
+                    <Image className={Wiki.img} src="../Matnnahoma/Matnnahoma.png" width={4095} height={4095} alt="Um mapa de Matnnahoma" />
                     <ItemWiki titulo=">" desc="O mapa da metrópole de Matnnahoma." /> </Link> <br />
-                <Link href="../Matnnahoma/Matnnahoma.png">
-                    <Image className={Wiki.img} src="../Matnnahoma/Hain'Ky.png" width={4095} height={4095} alt="Um mapa de Algihand" />
+                <Link href="../Matnnahoma/Hain'Ky.png">
+                    <Image className={Wiki.img} src="../Matnnahoma/Hain'Ky.png" width={4095} height={4095} alt="Um mapa de Hain'ky" />
                     <ItemWiki titulo=">" desc="O mapa do porto de Hain'Ky." /> </Link> <br />
-                <Link href="../Matnnahoma/Matnnahoma.png">
-                    <Image className={Wiki.img} src="../Matnnahoma/Jarven.png" width={4095} height={4095} alt="Um mapa de Algihand" />
+                <Link href="../Matnnahoma/Jarven.png">
+                    <Image className={Wiki.img} src="../Matnnahoma/Jarven.png" width={4095} height={4095} alt="Um mapa de Jarven" />
                     <ItemWiki titulo=">" desc="O mapa da cidade de Jarven." /> </Link> <br />
-                <Link href="../Matnnahoma/Matnnahoma.png">
-                    <Image className={Wiki.img} src="../Matnnahoma/Osteri.png" width={4095} height={4095} alt="Um mapa de Algihand" />
+                <Link href="../Matnnahoma/Osteri.png">
+                    <Image className={Wiki.img} src="../Matnnahoma/Osteri.png" width={4095} height={4095} alt="Um mapa de Osteri" />
                     <ItemWiki titulo=">" desc="O mapa do porto de Osteri." /> </Link > <br />
-                <Link href="../Matnnahoma/Matnnahoma.png">
-                    <Image className={Wiki.img} src="../Matnnahoma/Saarem'Ta.png" width={4095} height={4095} alt="Um mapa de Algihand" />
+                <Link href="../Matnnahoma/Saarem'Ta.png">
+                    <Image className={Wiki.img} src="../Matnnahoma/Saarem'Ta.png" width={4095} height={4095} alt="Um mapa de Saarem'Ta" />
                     <ItemWiki titulo=">" desc="O mapa da cidade da ilha de Saarem'Ta." /> </Link> <br />
-                <Link href="../Matnnahoma/Matnnahoma.png">
-                    <Image className={Wiki.img} src="../Matnnahoma/Valissa.png" width={4095} height={4095} alt="Um mapa de Algihand" />
+                <Link href="../Matnnahoma/Valissa.png">
+                    <Image className={Wiki.img} src="../Matnnahoma/Valissa.png" width={4095} height={4095} alt="Um mapa de Valissa" />
                     <ItemWiki titulo=">" desc="O mapa da cidade de Valissa." /> </Link>
             </div>
         </main>
